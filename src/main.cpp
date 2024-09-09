c8fc4ee5ef8d577043dc490e1498dc4309d3a7a8
albimazzu <albymazzu@yahoo.it>
2024-09-09 23:28:48 +0200

    Configurable time

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include "SPIFFS.h"
+#include <FS.h>
+#include <ArduinoJson.h> // JSON parsing
+
 #include "debounceInterrupt.h"
 #include "FireTimer.h"
 
@@ -15,23 +19,45 @@
 #define PIN_IN1   0
 #define PIN_IN2   19
 
+#define DEFAULT_TIME_fullShutterMove 10000
+#define CALIBRATION_PRESSED_TIME 5000 //msec, when up/down buttons are pressed more than this time start calibration
+
 // Istanza della classe DebounceInterrupt
 DebounceInterrupt debounceInterruptUp(0, 80, PIN_ACIN_1, 100000); //time in microseconds
 DebounceInterrupt debounceInterruptDown(1, 80, PIN_ACIN_2, 100000);
 
-FireTimer TIMER_CurtainMove;
+FireTimer TIMER_ShutterMove;
+FireTimer TIMER_RelaySpikeFilter;
 FireTimer TIMER_Heartbeat;
 
-bool OUT_CurtainUp = false;
-bool OUT_CurtainDown = false;
+bool IN_CommandUp = false;
+bool IN_CommandDown = false;
+bool IN_ACIN_3 = false;
+bool IN_ACIN_4 = false;
 
-int STEP_curtainMove = 0;
-int oldSTEP_curtainMove = 1;
+bool OUT_ShutterUp = false;
+bool OUT_ShutterDown = false;
+
+int STEP_shutterMove = 0;
+int oldSTEP_shutterMove = 1;
+
+unsigned long upDownPressedTime = 0;    //This variable count how much time up or down are kept pressed
+unsigned long TIME_fullShutterMove = 0; //Time in millis needed for complete shutter movement (open or close)
+
+void IO_sync();
+void heartbeat();
+void stateMachine();
+
+//SPIFFS functions
+void setupSPIFFS();
+void loadConfig();
+void saveConfig();
+
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("ESPcurtain startup");
+  Serial.println("ESPshutter startup");
   pinMode(PIN_UP_CMD, OUTPUT);
   pinMode(PIN_DOWN_CMD, OUTPUT);
   pinMode(PIN_STATUSLED, OUTPUT);
@@ -44,85 +70,165 @@ void setup() {
   pinMode(PIN_SENSE, INPUT);
   pinMode(PIN_USRBTN, INPUT);
 
-  TIMER_CurtainMove.begin(23000);
-  TIMER_Heartbeat.begin(500);
-  
+  TIMER_Heartbeat.begin(500);  
+  TIMER_RelaySpikeFilter.begin(200);
+
+
+  setupSPIFFS();
+  loadConfig();  // Load config.json var values
 }
 
 
 void loop() {
 
+  heartbeat();
+  IO_sync();
+  stateMachine();
+
+}
+
+void stateMachine()
+{
+
+  if(STEP_shutterMove != oldSTEP_shutterMove)
+  {
+    oldSTEP_shutterMove = STEP_shutterMove;
+    Serial.println("STEP_shutterMove="+String(STEP_shutterMove));
+  }
+
+  switch (STEP_shutterMove)
+  {
+    //IDLE
+    case 0:    
+      OUT_ShutterDown = false;
+      OUT_ShutterUp = false;
+      TIMER_ShutterMove.stop();
+      if(IN_CommandDown != IN_CommandUp)
+      {
+        OUT_ShutterUp = IN_CommandUp;
+        OUT_ShutterDown = IN_CommandDown;
+        upDownPressedTime = millis();
+        TIMER_ShutterMove.begin(TIME_fullShutterMove);
+        TIMER_ShutterMove.start();
+        STEP_shutterMove = 1;
+      }
+    break;
+
+    //wait button release
+    case 1:
+      //If button is pressed more than CALIBRATION_PRESSED_TIME, start calibration process.
+      if(millis()- upDownPressedTime > CALIBRATION_PRESSED_TIME)
+      {
+        Serial.println("Starting time calibration");
+        STEP_shutterMove = 100;
+      }
+      if(!IN_CommandDown && !IN_CommandUp)
+        STEP_shutterMove = 2;
+    break;
+
+    //Check stop action
+    case 2:
+      if(TIMER_ShutterMove.fire() || IN_CommandDown || IN_CommandUp)
+      {
+        TIMER_ShutterMove.stop();
+        OUT_ShutterUp = false;
+        OUT_ShutterDown = false;
+        STEP_shutterMove = 3;
+      }
+    break;        
+
+    //Relay spike filtering
+    case 3:
+      TIMER_RelaySpikeFilter.start();
+      if(!IN_CommandDown && !IN_CommandUp && TIMER_RelaySpikeFilter.fire())
+      {
+        TIMER_RelaySpikeFilter.stop();        
+        STEP_shutterMove = 0;
+      }
+    break;    
+
+    //calibration phase
+    case 100:
+      if(!IN_CommandDown && !IN_CommandUp)
+      {
+        TIME_fullShutterMove  = millis()-upDownPressedTime;
+        Serial.println("New TIME_fullShutterMove value:"+String(TIME_fullShutterMove));
+        saveConfig();
+        STEP_shutterMove = 3;
+      }
+      break;
+  
+    default:
+      Serial.println("bad STEP_shutterMove!");
+      STEP_shutterMove = 0;
+      break;
+  }
+
+}
+
+void heartbeat()
+{
   TIMER_Heartbeat.start();
   if(TIMER_Heartbeat.fire())
   {
     TIMER_Heartbeat.stop();
     digitalWrite(PIN_STATUSLED,!digitalRead(PIN_STATUSLED));
-  }
-  
-  digitalWrite(PIN_UP_CMD, OUT_CurtainUp);
-  digitalWrite(PIN_DOWN_CMD, OUT_CurtainDown);
-
-  if(STEP_curtainMove != oldSTEP_curtainMove)
-  {
-    oldSTEP_curtainMove = STEP_curtainMove;
-    Serial.println("STEP_curtainMove="+String(STEP_curtainMove));
-  }
-
-  switch (STEP_curtainMove)
-  {
-    //IDLE
-    case 0:    
-      OUT_CurtainDown = false;
-      OUT_CurtainUp = false;
-      TIMER_CurtainMove.stop();
-      if(debounceInterruptDown.isPressed())
-        STEP_curtainMove = 2;
-      else if(debounceInterruptUp.isPressed())
-        STEP_curtainMove = 1;
-    break;
-
-    //Curtain UP
-    case 1:
-      OUT_CurtainUp = true;
-      TIMER_CurtainMove.start();
-      STEP_curtainMove = 3;
-    break;
-
-    //Curtain DOWN
-    case 2:
-      OUT_CurtainDown = true;
-      TIMER_CurtainMove.start();
-      STEP_curtainMove = 3;
-    break;
-
-    //wait button release
-    case 3:
-      if(!debounceInterruptDown.isPressed() && !debounceInterruptUp.isPressed())
-        STEP_curtainMove = 4;
-    break;
-
-    //Check stop action
-    case 4:
-      if(TIMER_CurtainMove.fire() || debounceInterruptDown.isPressed() || debounceInterruptUp.isPressed())
-      {
-        TIMER_CurtainMove.stop();
-        OUT_CurtainUp = false;
-        OUT_CurtainDown = false;
-        STEP_curtainMove = 5;
-      }
-    break;    
-
-    //wait button release
-    case 5:
-      if(!debounceInterruptDown.isPressed() && !debounceInterruptUp.isPressed())
-      STEP_curtainMove = 0;
-    break;
-  
-  default:
-    break;
-  }
-
-
-  
+  }  
 }
 
+void IO_sync()
+{
+  digitalWrite(PIN_UP_CMD, OUT_ShutterUp);
+  digitalWrite(PIN_DOWN_CMD, OUT_ShutterDown);
+
+  IN_CommandUp = !digitalRead(PIN_USRBTN);
+  //IN_CommandUp = debounceInterruptUp.isPressed();
+  IN_CommandDown = debounceInterruptDown.isPressed();
+}
+
+// Init SPIFFS
+void setupSPIFFS() {
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Failed to mount SPIFFS");
+  } else {
+    Serial.println("SPIFFS mounted successfully");
+  }
+}
+
+//Loading config.json
+void loadConfig() {
+  File file = SPIFFS.open("/config.json", "r");
+  if (!file) {
+    Serial.println("Failed to open config file");
+    return;
+  }
+
+  JsonDocument doc;
+  DeserializationError error = deserializeJson(doc, file);
+  if (error) {
+    Serial.println("Failed to read config file, using default TIME_fullShutterMove");
+    TIME_fullShutterMove = DEFAULT_TIME_fullShutterMove;
+  } else {
+    TIME_fullShutterMove = doc["TIME_fullShutterMove"] | DEFAULT_TIME_fullShutterMove;
+    Serial.println("Loaded TIME_fullShutterMove: " + String(TIME_fullShutterMove));
+  }
+  file.close();
+}
+
+// Save data into config.json
+void saveConfig() {
+  File file = SPIFFS.open("/config.json", "w");
+  if (!file) {
+    Serial.println("Failed to open config file for writing");
+    return;
+  }
+
+  JsonDocument doc;
+  doc["TIME_fullShutterMove"] = TIME_fullShutterMove;
+  
+  if (serializeJson(doc, file) == 0) {
+    Serial.println("Failed to write to config file");
+  }
+  
+  file.close();
+}
